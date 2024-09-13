24c3cc8d1d158c91c81e1add1921832f8185fd97
Rob Fowler <rfo@mianos.com>
2024-09-13 16:33:42 +1000

    wifi config done and working

--- main/main.cpp
+++ main/main.cpp
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+#include "esp_log.h"
+
 #include "WifiManager.h"
 #include "SettingsManager.h"
+
+static const char *TAG = "npc";
 
 static SemaphoreHandle_t wifiSemaphore;
 
@@ -13,11 +17,18 @@ static void localEventHandler(void* arg, esp_event_base_t event_base, int32_t ev
 
 
 extern "C" void app_main() {
-{
 	NvsStorageManager nv;
 	SettingsManager settings(nv);
 
 	wifiSemaphore = xSemaphoreCreateBinary();
 	WiFiManager wifiManager(nv, localEventHandler, nullptr);
+//	wifiManager.clear();
+    if (xSemaphoreTake(wifiSemaphore, portMAX_DELAY) ) {
+		ESP_LOGI(TAG, "Main task continues after WiFi connection.");
+		while (true) {
+			vTaskDelay(pdMS_TO_TICKS(100)); 
+		
+		}
+	}
 
 }
